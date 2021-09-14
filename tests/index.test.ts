4a702e8a8b3debf53dcedc8c8bad2e119459c4de
Roger Qiu <roger.qiu@polyhack.io>
2021-09-15 00:36:47 +1000

    Removed index.test.ts

--- tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-// Import { Library } from '@';
-
-// describe('index', () => {
-//   test('some arbitrary test', () => {
-//     const library = new Library('some param');
-//     expect(library?.someParam).toEqual('some param');
-//   });
-// });
