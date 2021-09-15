fc455e831b58b0ad0a3b0c8022e924e1bd4c5ab2
Roger Qiu <roger.qiu@polyhack.io>
2021-09-15 16:44:53 +1000

    Added system information report

--- benches/index.ts
+++ benches/index.ts
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import si from 'systeminformation';
 import WorkerManagerBench from './WorkerManager.bench';
 
 async function main(): Promise<void> {
   await WorkerManagerBench();
+  const systemData = await si.get({
+    cpu: '*',
+    osInfo: 'platform, distro, release, kernel, arch',
+    system: 'model, manufacturer'
+  });
+  await fs.promises.writeFile(
+    'benches/results/system.json',
+    JSON.stringify(systemData, null, 2)
+  );
 }
 
 main();
