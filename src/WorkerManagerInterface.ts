1334c721358dc944da026f1b6757657375b78f67
Roger Qiu <roger.qiu@polyhack.io>
2022-04-11 14:59:49 +1000

    Destroy options should be optional in the interface

--- src/WorkerManagerInterface.ts
+++ src/WorkerManagerInterface.ts
@@ -3,7 +3,7 @@ import type { ModuleMethods } from 'threads/dist/types/master';
 import type { QueuedTask } from 'threads/dist/master/pool-types';
 
 interface WorkerManagerInterface<W extends ModuleMethods> {
-  destroy(options: { force?: boolean }): Promise<void>;
+  destroy(options?: { force?: boolean }): Promise<void>;
   call<T>(f: (worker: ModuleThread<W>) => Promise<T>): Promise<T>;
   queue<T>(
     f: (worker: ModuleThread<W>) => Promise<T>,
