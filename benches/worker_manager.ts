04e1aba4d41b21ad3fefc279f4a333533abb068a
Roger Qiu <roger.qiu@polyhack.io>
2022-07-22 16:10:12 +1000

    test: updated benchmarks to use 1 core to standardise between platforms, and updated benchmark names

--- benches/worker_manager.ts
+++ benches/worker_manager.ts
@@ -1,21 +1,18 @@
 import type { WorkerModule } from '@/worker';
-
-import os from 'os';
 import path from 'path';
 import crypto from 'crypto';
 import b from 'benny';
 import { spawn, Worker, Transfer } from 'threads';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import WorkerManager from '@/WorkerManager';
-import { suiteCommon } from './utils/utils';
+import { suiteCommon } from './utils';
 
 const logger = new Logger('WorkerManager Bench', LogLevel.WARN, [
   new StreamHandler(),
 ]);
 
 async function main() {
-  const cores = os.cpus().length;
-  logger.warn(`Cores: ${cores}`);
+  const cores = 1;
   const workerManager = await WorkerManager.createWorkerManager<WorkerModule>({
     workerFactory: () => spawn(new Worker('../src/worker')),
     cores,
@@ -26,7 +23,7 @@ async function main() {
   const bytes = crypto.randomBytes(1024 * 1024);
   const summary = await b.suite(
     path.basename(__filename, path.extname(__filename)),
-    b.add('Call Overhead', async () => {
+    b.add('call overhead', async () => {
       // This calls a noop, this will show the overhead costs
       // All parallelised operation can never be faster than this
       // Therefore any call that takes less time than the overhead cost
@@ -35,9 +32,9 @@ async function main() {
         await w.sleep(0);
       });
     }),
-    b.add('Parallel (2) Call Overhead', async () => {
-      // Assuming core count greater or equal to 2
-      // the performance should be similar to Call Overhead
+    b.add('parallel call overhead', async () => {
+      // Assuming core count is 1
+      // the performance should be half of `call overhead`
       await Promise.all([
         workerManager.call(async (w) => {
           await w.sleep(0);
@@ -47,7 +44,7 @@ async function main() {
         }),
       ]);
     }),
-    b.add('Parallel (2) Queue Overhead', async () => {
+    b.add('parallel queue overhead', async () => {
       // This should be slightly faster than using call
       // This avoids an unnecessary wrapper into Promise
       await Promise.all([
@@ -59,32 +56,32 @@ async function main() {
         }),
       ]);
     }),
-    b.add('JSON stringify of 1 MiB of data', () => {
+    b.add('json stringify of 1 MiB of data', () => {
       JSON.stringify(bytes);
     }),
-    b.add('Base64 of 1 MiB of data', () => {
+    b.add('base64 of 1 MiB of data', () => {
       bytes.toString('base64');
     }),
-    b.add('MD5 Hash of 1 MiB of data', () => {
+    b.add('md5 Hash of 1 MiB of data', () => {
       const hash = crypto.createHash('md5');
       return () => {
         hash.update(bytes);
       };
     }),
-    b.add('SHA256 Hash of 1 MiB of data', () => {
+    b.add('sha256 Hash of 1 MiB of data', () => {
       const hash = crypto.createHash('sha256');
       return () => {
         hash.update(bytes);
       };
     }),
-    b.add('SHA512 Hash of 1 MiB of data', () => {
+    b.add('sha512 Hash of 1 MiB of data', () => {
       // This will be faster on 64 bit machines compared to sha256
       const hash = crypto.createHash('sha512');
       return () => {
         hash.update(bytes);
       };
     }),
-    b.add('Transfer Overhead', async () => {
+    b.add('transfer Overhead', async () => {
       // This is the fastest possible ArrayBuffer transfer
       // First with a 1 MiB slice-copy
       // Then with a basic transfer to, and transfer back
@@ -97,7 +94,7 @@ async function main() {
         return Buffer.from(outputAB);
       });
     }),
-    b.add('Slice-Copy of 1 MiB of data', () => {
+    b.add('slice-Copy of 1 MiB of data', () => {
       // Compare this to Transfer Overhead
       bytes.buffer.slice(bytes.byteOffset, bytes.byteOffset + bytes.byteLength);
     }),
